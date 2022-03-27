8670c1ecf21d3f613799a5f45e32f853d03905f7
Luiz Silva <luizsilva.ee@gmail.com>
2022-03-27 17:07:38 -0300

    feat: creates Home

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 function App() {
+
   return (
     <div className="App">
       <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
