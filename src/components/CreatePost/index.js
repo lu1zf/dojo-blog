e82c951188ead5bc8f97ffb484e4afda2e7eb687
Luiz Silva <luizsilva.ee@gmail.com>
2022-03-28 13:28:35 -0300

    feat(post): submit new post

--- src/components/CreatePost/index.js
+++ src/components/CreatePost/index.js
@@ -1,7 +1,62 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import './styles.css';
+
 const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPost = { title, body, author };
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/posts', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    }).then(() => {
+      console.log('new post added');
+      setIsPending(false);
+      history.push('/');
+    })
+  }
+
   return (
     <div className="create">
       <h2>Add a new post</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(event) => setBody(event.target.value)}
+        ></textarea>
+        <label>Author:</label>
+        <select
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        {!isPending && <button>Add post</button>}
+        {isPending && <button disabled>Adding post...</button>}
+
+      </form>
     </div>
   );
 }
