1ffae38c6351f79ff12e3779fa19494dddfaff36
Luiz Silva <luizsilva.ee@gmail.com>
2022-03-28 00:31:47 -0300

    feat(navbar): update links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dojo Blog </h1>
+      <h1>The Dojo Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New post</a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New post</Link>
       </div>
     </nav>
   );
