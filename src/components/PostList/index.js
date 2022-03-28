0b72ac913a9303ab2fac7940a2d2c2fa89ecfda2
Luiz Silva <luizsilva.ee@gmail.com>
2022-03-28 12:35:57 -0300

    feat: insert posts links

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const PostList = ({ posts, title }) => {
   return (
     <div className="post-list">
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       {posts.map(post => (
-        <div className="blog-preview" key={post.id}>
-          <h2>{post.title}</h2>
-          <p>Author: {post.author}</p>
+        <div className="post-preview" key={post.id}>
+          <Link to={`/posts/${post.id}`}>
+            <h2>{post.title}</h2>
+            <p>Author: {post.author}</p>
+          </Link>
         </div>
       ))}
     </div>
